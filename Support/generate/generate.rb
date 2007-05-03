be3687dc39e3d1e5c2f616aeb47c0a20109a1423
Ciarán Walsh <github@ciaranwal.sh>
2007-05-03 15:37:27 +0000

    I rewrote this script entirely, to also include the PHP source parsing – pass the path to the php source package on the commandline. It now parses the PHP source and updates the Completions preferences plist, the functions.txt database and outputs the patterns for pasting into the language definition in TM. (I should make it update the language automatically if the plist gem is installed…) It now handles function aliases (such as is_int/is_integer → is_long), which was the reason for the rewrite.
    git-svn-id: http://svn.textmate.org/trunk/Bundles/PHP.tmbundle@7115 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/generate/generate.rb
+++ Support/generate/generate.rb
@@ -1,38 +1,123 @@
-#!/usr/bin/env ruby
-require 'enumerator' # each_slice
+#!/usr/bin/env ruby -wKU
+
+# Extract function summaries from sources of PHP and its extensions
+# 
+# Example of block looked in .c, .cpp, .h and .ec files
+# 
+# /* {{{ proto string zend_version(void)
+#   Get the version of the Zend Engine */
+# 
+
 require 'rubygems'
 require 'builder'
 
-# These are language constructs rather than functions, so don't appear in the generated list
-# includes/requires are in the language definiation so shouldn't be here
-ConstructFunctions = {
-  'eval'  => ['mixed eval(string code_str)', 'Evaluate a string as PHP code'],
-  'echo'  => ['void echo(string arg1 [, string ...])', 'Output one or more strings'],
-  'print' => ['int print(string arg)', 'Output a string'],
-  'empty' => ['bool empty(mixed var)', 'Determine whether a variable is empty'],
-  'isset' => ['bool isset(mixed var [, mixed var [, ...]])', 'Determine whether a variable is set'],
-  'unset' => ['void unset(mixed var [, mixed var [, ...]])', 'Unset a given variable'],
-  'list'  => ['void list(mixed varname, mixed ...)', 'Assign variables as if they were an array'],
-  'array' => ['array array([mixed ...])', 'Create an array'],
-  'exit' => ['void exit([string status])', 'Output a message and terminate the current script'],
-  'die' => ['void die([string status])', 'Output a message and terminate the current script'],
-}
+path = ARGV.join('')
+parsefiles = Dir[path + '**/*.{cpp,c,h,ec}']
 
-Keywords = ['if', 'else', 'elseif', 'endif',
-                'for', 'foreach', 'endforeach',
-                'while', 'do', 'endwhile',
-                'switch', 'endswitch', 'case', 'default',
-                'break', 'continue',
-                'function', 'class']
-
-if ARGV.size == 0
-  puts "Usage: generate.rb functions_list.txt"
-  puts "Run genfuncsummary.php (available on PHP CVS) to get functions_list.txt"
-  exit 1
+unless parsefiles
+  puts "No files found"
+  exit
 end
 
-functions_file = ARGV.join(' ')
+# These are language constructs rather than functions, so don't appear in the generated list
+ConstructFunctions = %w[eval echo empty isset unset print list array]
 
+# =======================
+# = Source file parsing =
+# =======================
+
+# "\n\n/* {{{ proto string zend_version(void)\n   Get the version of the Zend Engine */"
+proto_regex  = /^\s*?    # Start of the line
+                 \/\*    # Start of the comment
+                 \s+?   
+                 \{{3}   # 3 Braces
+                 \s+?
+                 proto
+                 \s+?
+                 (\S+?)?  # Type
+                 \s+?
+                 (.+?)         # Function name
+                 $             # End of the prototype line
+                 (.+?)         # Function description
+                \*\/      # End of the comment
+              /msx
+alias_regex  = /PHP_FALIAS\((\w+),\s*(\w+)/
+
+functions = {}
+aliases = {}
+sections = {}
+classes = []
+
+# prototype blocks
+parsefiles.sort.each_with_index do |file, key|
+  file_contents = File.read(file)
+  if file_contents.match(proto_regex)
+    section = file.match(/^.+\/(?:zend_)?(.+)\..+$/).captures.first
+
+    file_contents.gsub(proto_regex) do |proto|
+      ret, rest, desc = $1, $2, $3
+      next unless rest[/(?:(\S+?)::)?(\w+)\s*\(/]
+      klass, name = $1, $2
+      if klass
+        # Class method
+        # Ignore the method but add class name to a list
+        classes << klass
+        next
+      end
+      desc = desc.strip.gsub(/\s+?/ms, ' ')
+      functions[name] = {:type => section, :return => ret, :description => desc, :prototype => ret + ' ' + rest}
+      sections[section] ||= []
+      sections[section] << name
+    end
+  end
+  if file_contents.match(alias_regex)
+    file_contents.gsub(alias_regex) do
+      aliases[$1] = $2
+    end
+  end
+end
+
+aliases.each_pair do |func_alias, func|
+  next unless functions[func]
+  functions[func_alias] = functions[func].dup
+  sections[functions[func][:type]] << func_alias
+end
+
+classes.uniq!
+
+# =======================
+# = Completions writing =
+# =======================
+
+completions = classes + functions.keys
+completions.sort!
+
+xml = Builder::XmlMarkup.new(:indent => 2, :target => File.open('../../Preferences/Completions.tmPreferences', 'w'))
+xml.instruct!
+xml.declare! :DOCTYPE, :plist, :public, "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"
+xml.plist :version => '1.0' do
+  xml.dict do
+    xml.key 'name'
+    xml.string 'Completions'
+    xml.key 'scope'
+    xml.string 'source.php'
+    xml.key 'settings'
+    xml.dict do
+      xml.key 'completions'
+      xml.array do
+        completions.each do |function_name|
+          xml.string function_name
+        end
+      end
+    end
+    xml.key 'uuid'
+    xml.string '2543E52B-D5CF-4BBE-B792-51F1574EA05F'
+  end
+end
+
+# ==================
+# = Syntax writing =
+# ==================
 #  process_list
 #  Created by Allan Odgaard on 2005-11-28.
 #  http://macromates.com/svn/Bundles/trunk/Bundles/Objective-C.tmbundle/Support/list_to_regexp.rb
@@ -70,82 +155,26 @@ def process_list(list)
   end
 end
 
-def print_pattern(name, list)
+def print_pattern(name, list, require_parens = true)
   return unless list = process_list(list)
   puts <<-END
 {	name = '#{ name }';
-match = '(?i)\\b#{ list }\\b';
+match = '(?i)\\b#{ list }\\b#{"(?=\\s*\\()" if require_parens}';
 },
   END
 end
 
-def add_completion_data(name, prototype, description)
-  @completion_data << [name, prototype, description].map{|s| s.to_s.strip }
+print_pattern('support.function.construct.php', ConstructFunctions, false)
+sections.sort.each do |(section, funcs)|
+  print_pattern('support.function.' + section + '.php', funcs)
 end
+print_pattern('support.class.builtin.php', classes)
 
-data = File.read(functions_file).strip
-@completion_data = []
-
-sections = data.split(/^#.+\/(?:zend_)?(.+)\..+$/)[1..-1]
-
-classes     = [] # Stored class names, for a pattern at the end
-completions = [] # List of all function names for completion
-
-ConstructFunctions.each do |(function, info)|
-  add_completion_data(function, info[0], info[1])
-end
-
-sections.each_slice(2) do |(section, functions)|
-  functions = functions.strip.split(/$/)
-  function_names = []
-  functions.each_slice(2) do |(prototype, description)|
-    if prototype =~ /(\w+)::/
-      # Class method
-      # Ignore the method but add class name to a list
-      classes << $1
-    else
-      if prototype.strip.match(/(\w+)\s*\(/)
-        function_names << $1 
-        add_completion_data($1, prototype, description)
-      end
-    end
-  end
-  completions += function_names
-  print_pattern("support.function.#{section}.php", function_names)
-end
-
-print_pattern('support.function.construct', ConstructFunctions.keys)
-print_pattern('support.class.builtin', classes)
-
-@completion_data = @completion_data.sort_by {|function| function[0] }
+# =========================
+# = Functions.txt writing =
+# =========================
 File.open('../functions.txt', 'w') do |file|
-  @completion_data.each do |function|
-    file.puts function.join('%')
-  end
-end
-
-completions += Keywords + ConstructFunctions.keys
-completions.uniq!
-
-xml = Builder::XmlMarkup.new(:indent => 2, :target => File.open('../../Preferences/Completions.tmPreferences', 'w'))
-xml.instruct!
-xml.declare! :DOCTYPE, :plist, :public, "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"
-xml.plist :version => '1.0' do
-  xml.dict do
-    xml.key 'name'
-    xml.string 'Completions'
-    xml.key 'scope'
-    xml.string 'source.php'
-    xml.key 'settings'
-    xml.dict do
-      xml.key 'completions'
-      xml.array do
-        completions.each do |function_name|
-          xml.string function_name
-        end
-      end
-    end
-    xml.key 'uuid'
-    xml.string '2543E52B-D5CF-4BBE-B792-51F1574EA05F'
+  functions.sort.each do |function, data|
+    file << function + '%' + data[:prototype] + '%' + data[:description] + "\n"
   end
 end
